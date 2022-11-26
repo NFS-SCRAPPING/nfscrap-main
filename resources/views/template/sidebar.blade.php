d4ba8ae78699804b189fad87edd92b390f46b0bb
nifist99 <nikofigit@gmail.com>
2022-11-27 00:18:21 +0700

    menu admnin

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -56,6 +56,7 @@
       <li class="nav-item nav-category">
         <span class="nav-link">Navigation</span>
       </li>
+
       <li class="nav-item menu-items">
         <a class="nav-link" href="index.html">
           <span class="menu-icon">
@@ -64,79 +65,87 @@
           <span class="menu-title">Dashboard</span>
         </a>
       </li>
-      <li class="nav-item menu-items">
-        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-          <span class="menu-icon">
-            <i class="mdi mdi-laptop"></i>
-          </span>
-          <span class="menu-title">Basic UI Elements</span>
-          <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="ui-basic">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-          </ul>
-        </div>
+
+      <li class="nav-item nav-category">
+        <span class="nav-link">Panel Admin</span>
       </li>
+
       <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/forms/basic_elements.html">
+        <a class="nav-link" href="index.html">
           <span class="menu-icon">
-            <i class="mdi mdi-playlist-play"></i>
+            <i class="mdi mdi-key"></i>
           </span>
-          <span class="menu-title">Form Elements</span>
+          <span class="menu-title">Roles Management</span>
         </a>
       </li>
+
       <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/tables/basic-table.html">
+        <a class="nav-link" href="index.html">
           <span class="menu-icon">
-            <i class="mdi mdi-table-large"></i>
+            <i class="mdi mdi-account-multiple"></i>
           </span>
-          <span class="menu-title">Tables</span>
+          <span class="menu-title">Users Management</span>
         </a>
       </li>
+
       <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/charts/chartjs.html">
+        <a class="nav-link" href="index.html">
           <span class="menu-icon">
-            <i class="mdi mdi-chart-bar"></i>
+            <i class="mdi mdi-widgets"></i>
           </span>
-          <span class="menu-title">Charts</span>
+          <span class="menu-title">Settings</span>
         </a>
       </li>
+
       <li class="nav-item menu-items">
-        <a class="nav-link" href="pages/icons/mdi.html">
+        <a class="nav-link" href="index.html">
           <span class="menu-icon">
-            <i class="mdi mdi-contacts"></i>
+            <i class="mdi mdi-flag-outline"></i>
           </span>
-          <span class="menu-title">Icons</span>
+          <span class="menu-title">Log Users Access</span>
         </a>
       </li>
-      <li class="nav-item menu-items">
-        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-          <span class="menu-icon">
-            <i class="mdi mdi-security"></i>
-          </span>
-          <span class="menu-title">User Pages</span>
-          <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="auth">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-          </ul>
-        </div>
+
+
+      <li class="nav-item nav-category">
+        <span class="nav-link">Panel Superadmin</span>
       </li>
+
       <li class="nav-item menu-items">
-        <a class="nav-link" href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
+        <a class="nav-link" href="index.html">
           <span class="menu-icon">
-            <i class="mdi mdi-file-document-box"></i>
+            <i class="mdi mdi-format-list-bulleted-type"></i>
           </span>
-          <span class="menu-title">Documentation</span>
+          <span class="menu-title">Menu Management</span>
         </a>
       </li>
+
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="index.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-code-not-equal-variant"></i>
+          </span>
+          <span class="menu-title">Module Generator</span>
+        </a>
+      </li>
+
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="index.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-source-merge"></i>
+          </span>
+          <span class="menu-title">API Generator</span>
+        </a>
+      </li>
+
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="index.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-email"></i>
+          </span>
+          <span class="menu-title">Email Templates</span>
+        </a>
+      </li>
+
     </ul>
   </nav>
