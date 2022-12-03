0bcd294875bf419a5319d1883a83ddbac3710000
nifist99 <nikofigit@gmail.com>
2022-12-03 11:00:24 +0700

    cms

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function save_data($request){
+    public static function save_data($request){
 
         $save = User::insert([
             "name"          =>$request->name,
